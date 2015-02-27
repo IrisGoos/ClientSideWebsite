f623fddc3a40f791ac2543120d83b2663f7514d6
gmlent <goosvanlent@gmail.com>
2015-02-27 12:47:49 +0100

    Updates

--- Flickr.js
+++ Flickr.js
@@ -6,7 +6,7 @@
 function showpics(){
 
     var pic= $("#box").val();
-    $.getJSON('http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=49c116ba02e477c69e5903a4a18e0b17&tags=cat&tag_mode=OR',
+    $.getJSON('http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=49c116ba02e477c69e5903a4a18e0b17&tags=cat&tag_mode=OR&format=json',
       function(data){ $("#images").hide().html(data).fadeIn('fast');
 
     $.each(data.items, function(i,item) {
