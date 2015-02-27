f25986c4ac02cbf458d1419cae20e7302a4bdbb8
gmlent <goosvanlent@gmail.com>
2015-02-27 12:18:41 +0100

    Flickr update

--- Flickr.js
+++ Flickr.js
@@ -3,5 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+function showpics(){
 
+    var pic= $("#box").val();
+    $.getJSON('http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=49c116ba02e477c69e5903a4a18e0b17&tags=cat&tag_mode=OR',
+      function(data){ $("#images").hide().html(data).fadeIn('fast');
 
+    $.each(data.items, function(i,item) {
+      $("<img/>").attr("src", item.media.m).appendTo("#images");
+    });
+  })
+};
